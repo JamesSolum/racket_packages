aad56e775a50670014ae2e97c4ec58f52b9543af
Caleb Armacost <carmacost@westmont.edu>
2017-09-05 10:55:27 -0700

    reindent

--- math/math.rkt
+++ math/math.rkt
@@ -12,20 +12,20 @@ Figure out how we include this package in our racket files
 ;; negate : number -> number
 ;; negates a number
 (define (negate x)
-	(* -1 x))
+  (* -1 x))
 
 ;#### NEGATIVE?/POSITVE? #####
 ;; pos? : number -> bool
 ;; checks if a number is positive
 (define (pos? x)
-	(if (> x 0)
-		true
-		false))
+  (if (> x 0)
+      true
+      false))
 
 ;; neg? : number -> bool
 ;; checks if a number is negative
 (define (neg? x)
-	(not (pos? x)))
+  (not (pos? x)))
 
 ;##### EXPONENT #####
 ; HELPER
@@ -33,35 +33,35 @@ Figure out how we include this package in our racket files
 ;; raises x to the e power
 (define (posExp x e)
   (cond [(= e 0) 1]
-	[else (* x (posExp x (sub1 e)))]
-	))
+        [else (* x (posExp x (sub1 e)))]
+        ))
 
 ;; negExp : number number -> number
 ;; raises x to the negative e power
 (define (negExp x e)
-	(/ 1 (posExp x (negate e))))
+  (/ 1 (posExp x (negate e))))
 
 ;; exp : number number -> number
 ;; combines pos and neg exp
 (define (exp x e)
-	(if (pos? e)
-		(posExp x e)
-(negExp x e)))
+  (if (pos? e)
+      (posExp x e)
+      (negExp x e)))
 
 ;##### ABSOLUTE VALUE #####
 ;; abs : number -> number
 ;; takes any number and returns the absolute 
 ;; value of that number
 (define (abs x)
-	(if (< x 0)
-		(negate x)
-		x))
+  (if (< x 0)
+      (negate x)
+      x))
 
 ;##### AVERAGE #####
 ;; avg : lon -> number 
 ;; calculates the average of a list of numbers
 (define (avg lon)
-	(/ (foldr + 0 lon) (length lon)))
+  (/ (foldr + 0 lon) (length lon)))
 
 
 ;##### Advanced Add #####
@@ -70,8 +70,8 @@ Figure out how we include this package in our racket files
 
 (define-syntax add
   (syntax-rules ()
-		((_ a ...)
-		 (add-helper (list a ...)))))
+    ((_ a ...)
+     (add-helper (list a ...)))))
 
 (define (sub-helper lon)
   (foldl - 0 lon))
